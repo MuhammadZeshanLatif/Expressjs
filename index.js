1178d55e0d487bbcd439bbcbbceb7cfac22052ac
Muhammad Zeeshan Latif <zeeshanmuhammad42201@gmail.com>
2023-10-20 08:37:30 +0500

    dd

--- index.js
+++ index.js
@@ -5,12 +5,37 @@ const errorPage=fs.readFileSync('error.html','utf-8');
 
 //make server with express
 const express = require('express');
+const { type } = require('os');
 const server = express();
-
-
-
-server.listen(8080,()=>{
-    console.log("Server start....")
+server.get('/',(req,res)=>{
+    res.json({type:'GET'});
 });
+server.post('/',(req,res)=>{
+    res.json({type:'POST'});
+});
+server.delete('/',(req,res)=>{
+    res.json({type:'DELETE'});
+});
+server.patch('/',(req,res)=>{
+    res.json({type:'PATCH'});
+})
 
+
+
+
+
+
+
+
+
+server.get('/demo',(req,res)=>
+{
+    //res.sendStatus(404);
+    //res.json(data);
+    res.send("Server start....")
+    //res.sendFile('C:/Users/Dell/OneDrive/Desktop/Expressjs/index.html');
+    //we can send also stras
+    //res.status(200).send("Server start....")
+});
+server.listen(8080)
 
